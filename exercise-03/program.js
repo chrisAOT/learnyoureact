938d8f76044e5ad87b5f03cc225c35d04ebaaf49
Chris Holland <christopherholland@uvic.ca>
2019-08-16 19:58:52 -0700

    Updated var to const

--- exercise-03/program.js
+++ exercise-03/program.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.set('port', (process.argv[2] || 3000));
 app.set('view engine', 'jsx');
