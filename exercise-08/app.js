fa4b107a25c35fc26d237a94f35cf03470b3fd0b
Chris Holland <christopherholland@uvic.ca>
2019-08-16 21:56:15 -0700

    Issues with exercise 8, need to solve

---
+++ exercise-08/app.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import TodoBox from './views/index.jsx';
+
+const data = JSON.parse(document.getElementById('initial-data').getAttribute('data-json'));
+
+ReactDOM.render(
+    <TodoBox data={data} />,
+    document.getElementById("app")
+);
